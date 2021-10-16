84810ec57e743f5b6b7184fb93797f3d1855af18
Kevin Hu <iamhongwei0417@gamil.com>
2021-10-16 19:39:58 +0800

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ngx-scanner-test';
+  availableDevices: MediaDeviceInfo[];
+  currentDevice: MediaDeviceInfo = null;
+  hasPermission = false;
+  result = '';
+
+  onCamerasFound(devices: MediaDeviceInfo[]): void {
+    this.availableDevices = devices;
+  }
+
+  onHasPermission(has: boolean): void {
+    this.hasPermission = has;
+  }
+
+  onScanSuccess(result: string): void {
+    this.result = result;
+  }
+
+  onDeviceSelectChange(selected: string): void {
+    const device = this.availableDevices.find((x) => x.deviceId === selected);
+    this.currentDevice = device || null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ZXingScannerModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
